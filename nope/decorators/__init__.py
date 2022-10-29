b6c8c84a14c3ae107e85703ad22a165aea85e299
Martin Karkowski <m.karkowski@dozema.local>
2022-10-29 07:53:20 +0200

    Adding decorators

---
+++ nope/decorators/__init__.py
@@ -0,0 +1,1 @@
+from .classes import exportAsNopeService, exportAsNopeProperty, exportAsNopeEvent
---
+++ nope/decorators/classes.py
@@ -0,0 +1,41 @@
+from ..helpers import ensureDottedAccess
+
+def _class_decorator_factory(storing_type: str):
+
+    def func(options):
+
+        options = ensureDottedAccess(options)
+
+        class class_decorator:
+            """ A Simple Decorator for classes
+                This will store the decorated element in an list
+                named "decoratedItems"
+
+                You can provide options etc.
+            """
+
+            def __init__(self, item):
+                nonlocal storing_type
+                self._type = storing_type
+
+            def __set_name__(self, owner, name):
+                # We want to store the item.                
+                if not hasattr(owner, "decoratedItems"):
+                    setattr(owner, "decoratedItems", [])
+
+                # Store item
+                owner.decoratedItems.append(
+                    ensureDottedAccess({
+                        "accessor": name,
+                        "options": options,
+                        "type": self._type,
+                    })
+                )
+        
+        return class_decorator
+
+    return func
+
+exportAsNopeService     = _class_decorator_factory("method")
+exportAsNopeProperty    = _class_decorator_factory("property")
+exportAsNopeEvent       = _class_decorator_factory("event")
